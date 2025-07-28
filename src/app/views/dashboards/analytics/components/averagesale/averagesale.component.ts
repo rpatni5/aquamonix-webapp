a410e24fb14f17cfb4d90b97b487325e3ddd0979
shivijhaaa <shivijha016@gmail.com>
2025-07-28 11:11:33 +0530

    changes

--- src/app/views/dashboards/analytics/components/averagesale/averagesale.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-    selector: 'app-averagesale',
-    imports: [],
-    templateUrl: './averagesale.component.html',
-    styles: ``
-})
-export class AveragesaleComponent {}
--- src/app/views/dashboards/analytics/components/sales-countries/sales-countries.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-    selector: 'app-sales-countries',
-    imports: [],
-    templateUrl: './sales-countries.component.html',
-    styles: ``
-})
-export class SalesCountriesComponent {}
